4fc1e04e95074e41d3e0a98df5a7f0e666297ac0
Corey Halpin <chalpin@scout.wisc.edu>
2025-03-05 12:02:51 -0600

    Consider both issuer and client_id when looking up registrations

--- src/lti/Database.php
+++ src/lti/Database.php
@@ -2,8 +2,6 @@
 namespace IMSGlobal\LTI;
 
 interface Database {
-    public function find_registration_by_issuer($iss);
+    public function find_registration_by_issuer($iss, ?string $client_id);
     public function find_deployment($iss, $deployment_id);
 }
-
-?>
